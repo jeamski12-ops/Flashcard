7068c3705b703e3779fc3a032d8500cde2ec7b7a
jeamski12 -ops <jeamski12@gmail.com>
2025-11-18 18:38:50 -0500

    Mon premier comit

---
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.flashcard
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+            val flashcardquestion=findViewById<TextView>(R.id.textView2)
+             val flashcardrepons=findViewById<TextView>(R.id.textView3)
+               flashcardquestion.setOnClickListener {
+                 flashcardquestion.visibility= View.INVISIBLE
+                 flashcardrepons.visibility= View.VISIBLE
+
+             }
+
+        }
+    }
